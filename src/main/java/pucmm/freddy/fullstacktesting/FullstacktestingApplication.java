659d8facaa98183c27dbfb5b109938cffe39bdc5
Anthony Rosario G. <AXRG0002@ce.pucmm.edu.do>
2026-06-10 09:08:15 -0400

    feat: scaffold spring boot application

---
+++ src/main/java/pucmm/freddy/fullstacktesting/FullstacktestingApplication.java
@@ -0,0 +1,13 @@
+package pucmm.freddy.fullstacktesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstacktestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstacktestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pucmm/freddy/fullstacktesting/FullstacktestingApplicationTests.java
@@ -0,0 +1,13 @@
+package pucmm.freddy.fullstacktesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstacktestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
