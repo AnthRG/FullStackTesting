2508abdf81a4012e0e8951387653eb8ca0992f8e
Anthony Rosario G. <AXRG0002@ce.pucmm.edu.do>
2026-06-14 23:38:52 -0400

    test: add cucumber integration tests for auth and roles

--- src/test/java/pucmm/freddy/fullstacktesting/CucumberSpringConfiguration.java
+++ src/test/java/pucmm/freddy/fullstacktesting/CucumberSpringConfiguration.java
@@ -1,10 +1,12 @@
 package pucmm.freddy.fullstacktesting;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 
 @CucumberContextConfiguration
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfiguration {
 }
